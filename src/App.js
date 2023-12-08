88aaca72c5a8339fdff4e996eae78141bd0a54ba
YourGitHubUsername <shreegajanana.ent2021@gmail.com>
2023-12-08 10:54:30 +0530

    home updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
-
 import Footer from './components/footer'
-
 import Topnav from './components/topnav'
-import Navbar from './components/navbar'
+import Home from './pages/home'
+
 const App = () => {
   return (
    <>
    <Topnav/>
+   <Home/>
    <Footer/>
-
-   {/* <Navbar/> */}
    </>
   )
 }
--- src/components/topnav.js
+++ src/components/topnav.js
@@ -1,28 +1,7 @@
-// import React from 'react';
+
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
-
-// const TopNav = () => {
-//   return (
-//     <nav className='bg-gray-800 py-1 px-4 md:px-8'>
-//       <div className='container sticky mx-auto flex justify-between items-center'>
-//         <div className='text-white flex items-center'>
-//           <FontAwesomeIcon icon={faPhone} />
-//           <span className='ml-2'>1234543-432543</span>
-//         </div>
-
-      //   <div className='text-white flex items-center'>
-      //     <FontAwesomeIcon icon={faMapMarkerAlt} />
-      //     <span className='ml-2'>Hyderabad</span>
-      //   </div>
-      // </div>
-//     </nav>
-//   );
-// };
-
-// export default TopNav;
-import React from 'react'
-
 const topnav = () => {
   return (
     <div className='bg-gray-800 w-full sticky'>
